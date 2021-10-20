a50f89f7905874115c00d136386c6566bee47aa5
bharath sudharsan <bhasud@ie.deri.local>
2021-10-20 08:45:30 +0100

    Added full digits dataset as .h file

---
+++ Opt-OVO/Multiclass_digits_full_dataset/test.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67372ff99fd419e82813f4d9ac0ea9f30595910d5316230b84641a376a4cce46
+size 26129485
---
+++ Opt-OVO/Multiclass_digits_full_dataset/train.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7e881a174d48f48082924b6b2bc0de3c710d50a62e11c30ee83501f8060029a
+size 156616037
